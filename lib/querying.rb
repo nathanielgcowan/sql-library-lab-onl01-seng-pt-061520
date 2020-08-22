cdc7ae214a1a064fca060880daa8b42dd6f0d282
nathanielgcowan <nathaniel.g.cowan@gmail.com>
2020-08-22 02:01:05 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -26,11 +26,4 @@ def select_character_names_and_number_of_books_they_are_in
   JOIN characters 
   ON character_books.character_id = characters.id 
   GROUP BY characters.name ORDER BY book_count DESC, characters.name ASC"
-  
-    "SELECT characters.name, 
-    COUNT(*) as book_count 
-    FROM character_books 
-    JOIN characters 
-    ON character_books.character_id = characters.id
-    GROUP BY characters.name ORDER BY book_count DESC, characters.name ASC"
 end
